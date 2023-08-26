bbd2ac9845486b1a69fc85afc3e700e0d4acaf9c
Alexandr Neveroff <neveroff1976@gmail.com>
2023-08-26 14:53:37 +0300

    Swagger update

--- models/user.js
+++ models/user.js
@@ -124,11 +124,11 @@ export const helpEmailSchema = Joi.object({
     "string.base": "Field email must be a string",
     "string.pattern.base": "Field email not valid",
   }),
-  comment: Joi.string().min(6).max(500).required().messages({
+  comment: Joi.string().min(6).max(256).required().messages({
     "any.required": "Missing required comment field",
     "string.base": "Comment must be a string",
     "string.min": "Comment must be at least 6 characters long",
-    "string.max": "Comment must be no more than 500 characters long",
+    "string.max": "Comment must be no more than 256 characters long",
   }),
 });
 
