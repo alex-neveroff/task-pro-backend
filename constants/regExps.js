b53a1337b923980c07b4c5fcc515cf0673ce0186
Alexandr Neveroff <neveroff1976@gmail.com>
2023-08-30 22:10:15 +0300

    Comments deleted

--- constants/regExps.js
+++ constants/regExps.js
@@ -1,5 +1,3 @@
 export const emailRegExp = /^[a-zA-Z0-9.\-_]+@[a-zA-Z]+\.[a-zA-Z]+$/;
 export const passwordRegExp = /^[a-zA-Z0-9\-!@#$%^&*()_+,.:;'"?/]+$/;
 export const nameRegExp = /^[a-zA-Z0-9 !@#$%^&*()_+,.:;'"?/-]+$/;
-// export const deadlineRegExp =
-//   /^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/\d{4}$/;
--- middlewars/isEmptyBody.js
+++ middlewars/isEmptyBody.js
@@ -3,11 +3,7 @@ import { HttpError } from "./index.js";
 const isEmptyBody = (req, res, next) => {
   const { length } = Object.keys(req.body);
   if (!length) {
-    if (req.method === "PATCH") {
-      next(HttpError(400, "Missing field favorite"));
-    } else {
-      next(HttpError(400, "Missing fields"));
-    }
+    next(HttpError(400, "Missing fields"));
   }
   next();
 };
